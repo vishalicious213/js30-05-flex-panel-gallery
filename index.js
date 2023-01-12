85fec24114747b38cde94fcc6d598e6534c6cfbf
vishalicious213 <vishalicious213@gmail.com>
2023-01-12 15:23:28 -0800

    added in function to bring in the top and bottom rows AFTER panel's transition animation is complete

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ function toggleOpen() {
     this.classList.toggle("open")
 }
 
+function toggleActive(e) {
+    console.log(e.propertyName)
+    // using .includes("flex" instead of === "flex-grow" because Safari calls flex-grow flex)
+    if (e.propertyName.includes("flex")) {
+        this.classList.toggle("open-active")
+    }
+}
+
 panels.forEach(panel => panel.addEventListener("click", toggleOpen))
+
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive))
