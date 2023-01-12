ab221275e4606b90ab2fadf8b3a5789ed5c4ad72
vishalicious213 <vishalicious213@gmail.com>
2023-01-12 15:14:32 -0800

    grabbed all .panel elements, drafted function to toggle 'open' class and added eventListener to panels to toggle class on click

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Flex Panel Display")
+const panels = document.querySelectorAll(".panel") /* get nodelist of all .panels */
+
+function toggleOpen() {
+    this.classList.toggle("open")
+}
+
+panels.forEach(panel => panel.addEventListener("click", toggleOpen))
